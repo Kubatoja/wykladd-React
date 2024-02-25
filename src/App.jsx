9b458ed134bd721dd7c5efa09ab94eb781731caf
Kubatoja <jakub1720@gmail.com>
2024-02-25 18:19:30 +0100

    wwww
    ww

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
 import Swiatla from "./components/Swiatla";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="page">
       <Swiatla startColor={"green"} />
-      <Swiatla />
-      <Swiatla />
     </div>
   );
 }
